973847b062b56ff9fc006c54ff038d6ed958b383
Bernardo Araujo Alves <bernardoalves.estagiario@novacap.df.gov.br>
2023-11-13 13:59:20 -0300

    Começando a manipular dados.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,24 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventController extends Controller
 {
     public function index() {
-        $nome = "Matheus";
-        $idade = 29;
+        
+        $events = Event::all();
 
-        $arr = [10, 20, 30, 40, 50];
+        return view('welcome',['events' => $events]);
 
-        $nomes = ["Matheus", "Maria", "João", "Saulo"];
-
-        return view('welcome',
-            [
-                'nome' => $nome,
-                'idade2' => $idade,
-                'profissao' => "Programador",
-                'arr' => $arr,
-                'nomes' => $nomes
-            ]);
         }
 
     public function create() {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,42 +4,8 @@
 
 @section('content')
 
-<h1>Algum Título</h1>
-<img src="/img/banner.jpg" alt="Banner">
-
-<h1>Algum Título</h1>
-<img src="/img/event.jpg" alt="Banner Triste">
-@if(10 > 15)
-    <p>A condição é TRUE</p>
-@endif
-
-<p>{{ $nome }}</p>
-
-@if($nome == "Bernardo")
-    <p>O nome é Bernardo</p>
-@elseif($nome == "Matheus")
-    <p>O nome é {{ $nome }} e ele tem {{ $idade2 }} anos e trabalha como {{ $profissao }}</p>
-@else
-    <p>O nome não é Bernardo</p>
-@endif
-
-@for($i = 0; $i < count($arr); $i++)
-    <p>{{ $arr[$i] }} - {{ $i }}</p>
-    @if($i == 2)
-    <p>O i é 2</p>
-    @endif
-@endfor
-
-@foreach($nomes as $nome)
-    <p>{{ $loop->index }}</p>
-    <p>{{ $nome }}</p>
+@foreach($events as $event)
+    <p>{{ $event->title }} -- {{ $event->description }}</p>
 @endforeach
-
-@php
-    $name = "Bernardo";
-    echo $name;
-@endphp
-
-{{-- comentário do BLADE --}}
 
 @endsection
