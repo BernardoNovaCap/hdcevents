c0415689c285b0f52aa08295a1bbd7a22ef5e8a1
Bernardo Araujo Alves <bernardoalves.estagiario@novacap.df.gov.br>
2023-11-21 16:45:39 -0300

    Dashboard
    Adicionando um Dashboard para todo usuário saber quais eventos já criou.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 </div>
 <div id="events-container" class="col-md-12">
     @if($search)
-    <h2>Buscando por:</h2>
+    <h2>Buscando por: {{ $search }}</h2>
     @else
     <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
@@ -22,8 +22,8 @@
         <div class="card col-md-3">
             <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
-                <p class="card-date"> {{ date('d/m/Y', strtotime($event->date)) }}</p>
-                <h5 class="card-title"> {{ $event->title }} </h5>
+                <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
                 <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
@@ -32,7 +32,7 @@
         @if(count($events) == 0 && $search)
             <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos</a></p>
         @elseif(count($events) == 0)
-        <p>Não há eventos disponíveis</p>
+            <p>Não há eventos disponíveis</p>
         @endif
     </div>
 </div>
